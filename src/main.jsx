3f6d5ee7a7e34035ed1ccb4b47b5b22e74275599
Murodulla <yokubjanovich@Murodullas-MacBook-Air.local>
2024-07-08 11:47:45 +0500

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import Layout from "./layout/layout.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Router as AppRouter } from "./routes"; // Import the Router component you created
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Layout />
+    <Router>
+      <AppRouter />
+    </Router>
   </React.StrictMode>
 );
--- src/pages/Question/question.jsx
+++ src/pages/Question/question.jsx
@@ -1,5 +1,38 @@
 import React from "react";
-import "./question.css";
+import "./questions.css";
+
+const questionsList = [
+  "Tartiblik va tartibsiz ro'yhatlar orasida qanday farq mavjud?",
+  "HTML da element deb nimaga aytiladi?",
+  "Qanday input turlarini bilasiz...",
+  "HTML da qanday ro'yhat turlari bor?",
+  "Textarea va input taglari orasidagi farq nima?",
+  "HTML da qanday media taglari mavjud?",
+  "HTML formalar nima uchun ishlatiladi?",
+  "Audio va video taglar qanday ishlatiladi?",
+  "HTML jadvalda colspan va rowspan qanday vazifa uchun ishlatiladi?",
+  "HTMLning saytdagi vazifasi nimadan iborat?",
+  "HTML da link yaratish uchun qaysi tag ishlatiladi?",
+  "Anchor(Link) tag (a) va uning atributlari qanday ishlatiladi?",
+  "HTML attribute deb nimaga aytiladi?",
+  "<textarea> tagiga uzunlik va balandlik berish uchun qanday atributlar mavjud?",
+  "HTML da matnlarni bo'limlarga ajratishni qanday usullari bor?",
+  "label tagini vazifasi nimadan iborat?",
+  "label tagini input tagiga nechta usul bilan bog'lash mumkin",
+  "HTML da image qo'shish uchun qaysi tag ishlatiladi?",
+  "Img tagining atributlari va ulardan qanday foydalaniladi?",
+  "Media(audio, video) taglarida loop atributi nima vazifa bajaradi?",
+  "Block va inline elementlar orasidagi farq nima?",
+  "HTML tag deb nimaga ataladi?",
+  "HTML da table tagi va uning bolalari qanday ishlatiladi?",
+  "<button type='reset'> ning vazifasi nimadan iborat",
+  "<thead> va <tbody> taglarining vazifasi nimadan iborat",
+  "<form> tagini action atributini vazifasi nimadan iborat?",
+  "<!Doctype html> saytdagi vazifasi nimadan iborat?",
+  "HTML head va body taglari orasidagi farq nima?",
+  "Juft va toq taglarni qanday farqlari bor?",
+  "Media(audio, video) taglarida controls atributi nima vazifa bajaradi",
+];
 
 export default function Question() {
   return (
