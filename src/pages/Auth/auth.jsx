7f0b92fe796202fcaab3aacad810583facf7477b
Murodulla <yokubjanovich@Murodullas-MacBook-Air.local>
2024-07-09 18:25:50 +0500

    last commit

--- src/pages/Auth/auth.jsx
+++ src/pages/Auth/auth.jsx
@@ -65,6 +65,7 @@ export default function Auth() {
             <option value="12:00-14:00">12:00 - 14:00</option>
             <option value="14:00-16:00">14:00 - 16:00</option>
             <option value="16:00-18:00">16:00 - 18:00</option>
+            <option value="18:00-20:00">18:00 - 20:00</option>
           </select>
         </label>
         <label>
